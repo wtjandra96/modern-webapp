ff6290029917fe29253430a991eac65d20854c60
wtjandra96 <wtjandra@usc.edu>
2020-04-10 16:59:19 -0700

    made response to clearly return an errors object

--- server/src/loaders/express.js
+++ server/src/loaders/express.js
@@ -21,30 +21,30 @@ module.exports = (app) => {
   app.use((err, req, res, next) => {
     if (err.name === "PayloadTooLargeError") {
       logger.error(`${err.name}: ${err.message} | ${new Date()}`);
-      return res.status(400).json([{ errorMessage: err.message }]);
+      return res.status(400).json({ errors: [{ errorMessage: err.message }] });
     }
 
     if (isCelebrate(err)) {
       const errDetails = err.joi.details;
-      const errs = [];
+      const errors = [];
       let route = "";
       for (let i = 0; i < errDetails.length; i += 1) {
         const currentError = errDetails[i];
         const [errorRoute, errorMessage] = currentError.message.split(":");
-        errs.push({ errorMessage });
+        errors.push({ errorMessage });
         if (!route) {
           route = errorRoute;
         }
       }
-      const errMsg = `${route}: ValidationError ${JSON.stringify(errs)} | ${new Date()}`;
+      const errMsg = `${route}: ValidationError ${JSON.stringify(errors)} | ${new Date()}`;
       logger.error(errMsg);
-      return res.status(400).json(errs);
+      return res.status(400).json({ errors: errors || err });
     }
     logger.error(
       `${err.message} ${JSON.stringify(err.errors)} | ${err.date}`
     );
 
     const { httpStatusCode, errors } = err;
-    return res.status(httpStatusCode || 500).json(errors || err);
+    return res.status(httpStatusCode || 500).json({ errors: errors || err });
   });
 };
