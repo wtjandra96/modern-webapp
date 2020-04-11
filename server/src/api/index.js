4934bbdd7619820350b40df33b8915c256a8b11e
wtjandra96 <wtjandra@usc.edu>
2020-04-11 13:35:53 -0700

    connected categories and posts route

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -1,10 +1,14 @@
 const router = require("express").Router;
 
 const auth = require("./routes/auth");
+const categories = require("./routes/categories");
+const posts = require("./routes/posts");
 
 module.exports = () => {
   const app = router();
   auth(app);
+  categories(app);
+  posts(app);
 
   return app;
 };
