db848f6b86c0d4023a36968f8ab2a51ea6884bf0
wtjandra96 <wtjandra@usc.edu>
2020-04-11 13:37:49 -0700

    added category, label, and post models

--- server/src/loaders/index.js
+++ server/src/loaders/index.js
@@ -4,6 +4,9 @@ const mongooseLoader = require("./mongoose");
 const logger = require("./logger");
 
 const UserModel = require("../models/User");
+const PostModel = require("../models/Post");
+const CategoryModel = require("../models/Category");
+const LabelModel = require("../models/Label");
 
 module.exports = async (app) => {
   await mongooseLoader();
@@ -14,7 +17,22 @@ module.exports = async (app) => {
     model: UserModel
   };
 
-  dependencyInjectorLoader({ models: [userModel] });
+  const categoryModel = {
+    name: "CategoryModel",
+    model: CategoryModel
+  };
+
+  const labelModel = {
+    name: "LabelModel",
+    model: LabelModel
+  };
+
+  const postModel = {
+    name: "PostModel",
+    model: PostModel
+  };
+
+  dependencyInjectorLoader({ models: [userModel, categoryModel, labelModel, postModel] });
   logger.info("Dependency injector loaded");
 
   expressLoader(app);
