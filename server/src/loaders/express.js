c86d3c01fc66ff628377bfb6a539de820466236f
wtjandra96 <wtjandra@usc.edu>
2020-04-11 13:35:28 -0700

    modified logger error messages

--- server/src/loaders/express.js
+++ server/src/loaders/express.js
@@ -1,11 +1,13 @@
+const container = require("typedi").Container;
 const express = require("express");
 const { isCelebrate } = require("celebrate");
 
 const config = require("../config");
 const routes = require("../api");
-const logger = require("./logger");
 
 module.exports = (app) => {
+  const logger = container.get("logger");
+
   app.get("/status", (req, res) => {
     res.status(200).send("Hello world!");
   });
@@ -20,31 +22,39 @@ module.exports = (app) => {
   // eslint-disable-next-line no-unused-vars
   app.use((err, req, res, next) => {
     if (err.name === "PayloadTooLargeError") {
-      logger.error(`${err.name}: ${err.message} | ${new Date()}`);
+      logger.error(`${err.name}: ${err.message}`);
       return res.status(400).json({ errors: [{ errorMessage: err.message }] });
     }
 
     if (isCelebrate(err)) {
       const errDetails = err.joi.details;
       const errors = [];
-      let route = "";
       for (let i = 0; i < errDetails.length; i += 1) {
-        const currentError = errDetails[i];
-        const [errorRoute, errorMessage] = currentError.message.split(":");
-        errors.push({ errorMessage });
-        if (!route) {
-          route = errorRoute;
-        }
+        errors.push({ errorMessage: errDetails[i].message });
       }
-      const errMsg = `${route}: ValidationError ${JSON.stringify(errors)} | ${new Date()}`;
-      logger.error(errMsg);
+      const errBody = {
+        status: 400,
+        body: req.body,
+        query: req.query,
+        params: req.params,
+        errors
+      };
+      const errMsg = `${req.method} ${req.url}: ValidationError`;
+      logger.error(`${errMsg} %o\n`, errBody);
       return res.status(400).json({ errors: errors || err });
     }
-    logger.error(
-      `${err.message} ${JSON.stringify(err.errors)} | ${err.date}`
-    );
 
     const { httpStatusCode, errors } = err;
+    const errBody = {
+      status: httpStatusCode,
+      body: req.body,
+      query: req.query,
+      params: req.params,
+      errors: errors || err
+    };
+    const errMsg = `${req.method} ${req.url}: ${err.name}`;
+    logger.error(`${errMsg} %o\n`, errBody);
+
     return res.status(httpStatusCode || 500).json({ errors: errors || err });
   });
 };
